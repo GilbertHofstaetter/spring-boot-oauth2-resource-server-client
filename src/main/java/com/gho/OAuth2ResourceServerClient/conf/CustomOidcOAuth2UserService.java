b53b6830e4b36cb174f5a19f4917e9cb0d441f43
GilbertHofstaetter <gilbert.hofstaetter@gmail.com>
2022-11-06 16:24:18 +0100

    Enhancement added

--- src/main/java/com/gho/OAuth2ResourceServerClient/conf/CustomOidcOAuth2UserService.java
+++ src/main/java/com/gho/OAuth2ResourceServerClient/conf/CustomOidcOAuth2UserService.java
@@ -1,6 +1,7 @@
 package com.gho.OAuth2ResourceServerClient.conf;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
@@ -17,9 +18,14 @@ import org.springframework.util.StringUtils;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Component
 public class CustomOidcOAuth2UserService extends OidcUserService {
+
+    @Value("${spring.security.oauth2.client.registration.keycloak.client-id}")
+    private String resourceId; //=client id
 
     @Autowired
     JwtDecoder jwtDecoderByJwkKeySetUri;
@@ -29,15 +35,9 @@ public class CustomOidcOAuth2UserService extends OidcUserService {
         OidcUser user = super.loadUser(userRequest);
 
         Jwt jwt = jwtDecoderByJwkKeySetUri.decode(userRequest.getAccessToken().getTokenValue());
-        Collection<? extends GrantedAuthority> extractetRoles = JwtAuthorityExtractor.extractResourceRoles(jwt, "springboot-microservice");
-        Set<GrantedAuthority> authoritySet = new HashSet<>();
-        extractetRoles.forEach(role -> {
-            authoritySet.add(role);
-        });
-        user.getAuthorities().forEach(role -> {
-            authoritySet.add(role);
-        });
-        user = getUser(userRequest, user.getUserInfo(), authoritySet);
+        Collection<? extends GrantedAuthority> extractetRoles = JwtAuthorityExtractor.extractResourceRoles(jwt, resourceId);
+        Set<GrantedAuthority> authorities = Stream.concat(extractetRoles.stream(), user.getAuthorities().stream()).collect(Collectors.toSet());
+        user = getUser(userRequest, user.getUserInfo(), authorities);
 
         return user;
     }
